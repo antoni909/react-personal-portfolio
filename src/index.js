a29739f4fb816a5f3f6b35ea1169177a95bdd7be
antoni <antoni.dev0@gmail.com>
2022-03-29 10:06:51 -0700

    add basic Browser Router, add About,Contact,Resume,Home,Nav components

--- src/index.js
+++ src/index.js
@@ -1,10 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import App from './App';
+import About from './About';
+import Nav from './Nav';
+import Resume from './Resume';
+import Contact from './Contact';
 
+const rootElement = document.getElementById('root')
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <BrowserRouter>
+    <Nav />
+    <Routes>
+      <Route path="/" element={ <React.StrictMode><App /></React.StrictMode>}
+      />
+      <Route path="about" element={ <About/> } />
+      <Route path="resume" element={ <Resume/> } />
+      <Route path="contact" element={ <Contact/> } />
+
+    </Routes>
+
+  </BrowserRouter>,
+  rootElement
 );
