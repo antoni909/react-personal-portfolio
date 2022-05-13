049fb4b9e33120cf2ce1372a2a6298c4c15eee68
antoni <antoni.dev0@gmail.com>
2022-05-13 15:59:18 -0700

    create new component called SocialMediaList to store previous menuList array and rendered social media icons in Nav Component

--- src/Navigation/Nav.jsx
+++ src/Navigation/Nav.jsx
@@ -1,41 +1,16 @@
 import { useState } from 'react'
 import Home from '../Home/Home'
 import Footer from '../Footer/Footer.jsx'
-import { Outlet, Link } from 'react-router-dom';
-import useStyles from "../Theme/themes";
+import { Outlet } from 'react-router-dom';
+import useStyles from '../Theme/themes';
+import SocialMediaList from './SocialMediaList'
 
 import Box from '@mui/material/Box';
 import CssBaseline from '@mui/material/CssBaseline';
-import List from '@mui/material/List';
-import ListItem from '@mui/material/ListItem';
-import ListItemIcon from '@mui/material/ListItemText';
-import ListItemText from '@mui/material/ListItemText';
 
 import IconButton from '@mui/material/IconButton';
 import Menu from '@mui/material/Menu';
 import { FaHamburger } from 'react-icons/fa';
-import { FcDocument, FcEngineering } from "react-icons/fc";
-
-const menuList = [
-  {
-    text: 'Projects', 
-    path: <Link style={{ textDecoration: 'none' }} 
-    to="/solo_projects">Projects</Link>,
-    menuIcon: <FcEngineering style={{ height: 40, width: 40 }}/>
-  },
-  {
-    text: 'Resume', 
-    path: <Link style={{ textDecoration: 'none' }} 
-    to="/resume">Resume</Link>,
-    menuIcon: <FcDocument style={{ height: 40, width: 40 }}/>
-  },
-]
-
-// DONE: Redesign drawer navigation to instead use a hamburger menu
-// DONE: Scroll-to feature 
-
-// TODO: Go-to feature
-// DONE: Remove Social Icons from Hamburger Menu, fix icons above the hamburger icon
 
 const Nav = () => {
   const classes = useStyles();
@@ -71,24 +46,9 @@ const Nav = () => {
           onClose={ handleClose }
         >
           <Box className={classes.hamburgerMenuContainer} >
-            <List>
-              {menuList.map(( item ) => (
-                <ListItem
-                  button
-                  className={classes.hamburgerMenuIconOnHover}
-                  key={item.text}
-                >
-                  <ListItemIcon>
-                    {item.menuIcon}
-                  </ListItemIcon>
-                  <ListItemText primary={item.path} />
-                </ListItem>
-              ))}
-            </List>
-
+            <SocialMediaList />
           </Box>  
         </Menu>
-
       </Box>
 
       <Outlet/>
---
+++ src/Navigation/SocialMediaList.jsx
@@ -0,0 +1,45 @@
+import useStyles from '../Theme/themes';
+
+import { Link } from 'react-router-dom';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemIcon from '@mui/material/ListItemText';
+import ListItemText from '@mui/material/ListItemText';
+import { FcDocument, FcEngineering } from "react-icons/fc";
+
+const socialMediaList = [
+  {
+    text: 'Projects', 
+    path: <Link style={{ textDecoration: 'none' }} 
+    to="/solo_projects">Projects</Link>,
+    menuIcon: <FcEngineering style={{ height: 40, width: 40 }}/>
+  },
+  {
+    text: 'Resume', 
+    path: <Link style={{ textDecoration: 'none' }} 
+    to="/resume">Resume</Link>,
+    menuIcon: <FcDocument style={{ height: 40, width: 40 }}/>
+  },
+]
+
+const SocialMediaList = () => {
+  const classes = useStyles();
+  return(
+    <List>
+    {socialMediaList.map(( item ) => (
+      <ListItem
+        button
+        className={classes.hamburgerMenuIconOnHover}
+        key={item.text}
+      >
+        <ListItemIcon>
+          {item.menuIcon}
+        </ListItemIcon>
+        <ListItemText primary={item.path} />
+      </ListItem>
+    ))}
+  </List>
+  )
+}
+
+export default SocialMediaList
