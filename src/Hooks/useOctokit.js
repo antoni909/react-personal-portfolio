d45ab56b3fa82e6550994eac9515f286f7139613
antoni <antoni.dev0@gmail.com>
2022-07-13 14:03:19 -0700

    branch first commit, rm not-needed comments from useOctokit file

--- src/Hooks/useOctokit.js
+++ src/Hooks/useOctokit.js
@@ -3,6 +3,7 @@ import { octokit } from '../Utils/gh';
 
 const cache = {}
 // TODO: refactor to use hook useRef()
+
 export const useOctokit = (url) => {
   const [data, setData] = useState([])
   const [isPending, setPending] = useState(true)
@@ -13,12 +14,10 @@ export const useOctokit = (url) => {
       let response
   
       if(cache[url]){
-        // console.log('cache hit')
         const data = cache[url]
         setData(data)
         setPending(false)
       }else{
-          // console.log('cache miss')
           response = await octokit.request(url, {org: 'org'})
           cache[url] = response.data
           if(response.status === 200){
